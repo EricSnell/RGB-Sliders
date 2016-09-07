47d0f307b40a8cad44fa6d5b040de4ed1e891752
Eric Snell <eric_snell21@yahoo.com>
2016-09-07 18:26:05 -0400

    "Added navigation"

--- RGBSliders/ViewController.swift
+++ RGBSliders/ViewController.swift
@@ -12,12 +12,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         // So the background isn't white on initial load, we call the updateBackgroundColor function
+        
         updateBackgroundColor()
+        
+        colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,7 +36,9 @@ class ViewController: UIViewController {
         let green = CGFloat(greenSlider.value)
         let blue = CGFloat(blueSlider.value)
         
-        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+
+        
     }
 
 }
